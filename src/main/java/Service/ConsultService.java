e444d9e3941a3249d33e57d058c6417239da34b2
albertoGF-dawt <albertoguerraforestier@alumno.ieselrincon.es>
2026-06-06 19:19:07 +0100

    fixes again

--- src/main/java/Service/ConsultService.java
+++ src/main/java/Service/ConsultService.java
@@ -1,4 +1,35 @@
 package Service;
 
+import DAO.BookingDAO;
+import DAO.MentorshipDAO;
+import DAO.UserDAO;
+import Model.Booking;
+import Model.Mentorship;
+
+import java.util.List;
+
 public class ConsultService {
+
+    private final MentorshipDAO mentorshipDAO = new MentorshipDAO();
+    private final BookingDAO bookingDAO = new BookingDAO();
+
+    // Tutorías disponibles por materia
+    public List<Mentorship> getTutoriasPorMateria(int subjectId) {
+        return mentorshipDAO.findBySubject(subjectId);
+    }
+
+    // Tutorías publicadas por un mentor
+    public List<Mentorship> getTutoriasPorMentor(int mentorId) {
+        return mentorshipDAO.findByMentor(mentorId);
+    }
+
+    // Reservas de un alumno
+    public List<Booking> getReservasPorAlumno(int alumnoId) {
+        return bookingDAO.findByAlumno(alumnoId);
+    }
+
+    // Total de horas ofrecidas (todas las tutorías)
+    public int getTotalHoras() {
+        return mentorshipDAO.findAll().stream().mapToInt(m -> m.getDuracion()).sum() / 60;
+    }
 }
