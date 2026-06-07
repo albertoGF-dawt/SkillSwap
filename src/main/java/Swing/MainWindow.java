464152d120a7e9f2a261d9a03b852048a91a3e05
haridianvergaraalonso-boop <haridianvergaraalonso@alumno.ieselrincon.es>
2026-06-07 16:12:33 +0100

    portada cute

--- src/main/java/Swing/MainWindow.java
+++ src/main/java/Swing/MainWindow.java
@@ -16,30 +16,14 @@ public class MainWindow extends JFrame {
         setJMenuBar(crearMenuBar());
 
         // Panel central de bienvenida
-        JPanel panelCentral = new JPanel();
-        panelCentral.setLayout(new BoxLayout(panelCentral, BoxLayout.Y_AXIS));
-
-        JLabel bienvenida = new JLabel("Te damos la bienvenida a Skillswap");
-        JLabel descripcion = new JLabel("Elige qué opción quieres elegir en el menú superior.");
-        JLabel gracias = new JLabel("Gracias por usar nuestros servicios.");
-
+        JPanel panelCentral = new JPanel(new BorderLayout());
+        JLabel bienvenida = new JLabel("Te damos la bienvenida a Skillswap", SwingConstants.CENTER);
+        JLabel descripcion = new JLabel("Elije que opción quieres elegir en el menú superior de la ventana.", SwingConstants.BOTTOM);
+        JLabel gracias = new JLabel("Gracias por usar nuestros servicios.", SwingConstants.TOP);
         bienvenida.setFont(new Font("Comfortaa", Font.BOLD, 24));
         descripcion.setFont(new Font("Comfortaa", Font.BOLD, 18));
         gracias.setFont(new Font("Comfortaa", Font.BOLD, 14));
-
-// Centrar cada label horizontalmente dentro del BoxLayout
-        bienvenida.setAlignmentX(Component.CENTER_ALIGNMENT);
-        descripcion.setAlignmentX(Component.CENTER_ALIGNMENT);
-        gracias.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        panelCentral.add(Box.createVerticalGlue()); // empuja hacia el centro
-        panelCentral.add(bienvenida);
-        panelCentral.add(Box.createRigidArea(new Dimension(0, 12))); // espaciado
-        panelCentral.add(descripcion);
-        panelCentral.add(Box.createRigidArea(new Dimension(0, 8)));
-        panelCentral.add(gracias);
-        panelCentral.add(Box.createVerticalGlue()); // empuja desde abajo
-
+        panelCentral.add(bienvenida, BorderLayout.CENTER);
         add(panelCentral);
 
         setVisible(true); // Siempre al final
