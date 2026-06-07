c1b693dcd78e922ebab2dff8f7580d344f9f8690
haridianvergaraalonso-boop <haridianvergaraalonso@alumno.ieselrincon.es>
2026-06-07 16:38:45 +0100

    portada cute

--- src/main/java/Swing/MainWindow.java
+++ src/main/java/Swing/MainWindow.java
@@ -18,7 +18,7 @@ public class MainWindow extends JFrame {
         // Panel central de bienvenida
         JPanel panelCentral = new JPanel(new GridLayout(3, 1)) {
             private final Image imagen = new ImageIcon(
-                    getClass().getResource("/images/dab809b4-d9d4-4e7e-b8d4-aee75e6bdf52.png")
+                    getClass().getResource("C:/Users/Usuario/IdeaProjects/SkillSwap/src/main/java/Swing/Images/dab809b4-d9d4-4e7e-b8d4-aee75e6bdf52.png")
             ).getImage();
 
             @Override
@@ -41,7 +41,7 @@ public class MainWindow extends JFrame {
         panelCentral.add(gracias);
 
         add(panelCentral);
-      
+
         setVisible(true); // Siempre al final
     }
 
